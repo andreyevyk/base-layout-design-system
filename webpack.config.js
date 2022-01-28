195431d98d015773af481fc18b7290a9194bebbc
Andrey <andreyevyk13@gmail.com>
2022-01-28 16:38:54 -0300

    fix: resolve remoteEntry

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,24 @@
 const { StorybookWebpackFederationPlugin } = require("storybook-webpack-federation-plugin");
-const  HtmlWebPackPlugin  = require("html-webpack-plugin");
 const TsconfigPathsPlugin = require( 'tsconfig-paths-webpack-plugin');
-
+const Dotenv = require('dotenv-webpack');
 const path = require("path")
+const process = require("process")
 
 module.exports = {
-  output: {
-    path: path.resolve(__dirname, "storybook-static/federation"),
-    publicPath: "//localhost:3030/federation/",
+  cache: false,
+  
+  mode: "development",
+  devtool: "source-map",
+
+  optimization: {
+    minimize: false,
   },
+
   resolve: {
     extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
     plugins: [new TsconfigPathsPlugin()]
   },
-  devServer: {
-    port: 4005,
-    historyApiFallback: true,
-  },
+
   module: {
     rules: [
       {
@@ -39,12 +41,19 @@ module.exports = {
       },
     ],
   },
+
+  output: {
+    path: path.resolve(__dirname, "storybook-static/federation"),
+    publicPath: `//localhost:3030/federation/`,
+  },
+
   plugins: [
     new StorybookWebpackFederationPlugin({
       name: "designSystem",
       files: { 
-        paths: ["./src/components/*.ts{,x}"],
+        paths: ["./src/**/*.ts{,x}"],      
       },
     }),
+    new Dotenv()
   ],
 };
