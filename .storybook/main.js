24f382ba8d2c9bff9dad273f5f7a50598f7442a8
Andrey <andreyevyk13@gmail.com>
2022-01-28 15:11:50 -0300

    chore: config storybook module federation

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,22 +1,50 @@
 const TsconfigPathsPlugin = require( 'tsconfig-paths-webpack-plugin');
+const { withStorybookModuleFederation} = require('storybook-module-federation');
 
-module.exports = {
+const storybookConfig = {
+  core: {
+    builder: 'webpack5',
+  },
   stories: [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
+  core: {
+    builder: 'webpack5',
+  },
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
   ],
   framework: "@storybook/react",
-  webpackFinal: async (config) => {
+  webpackFinal: (config) => {
     config.resolve.plugins = [
       ...(config.resolve.plugins || []),
       new TsconfigPathsPlugin({
         extensions: config.resolve.extensions,
       }),
+      
     ];
     return config;
   }
-}
+};
+const moduleFederationConfig = {
+  name: "design_system",
+  filename: "remoteEntry.js",
+  remotes: {},
+  exposes: {
+    "./Button": "./src/components/Button"
+  },
+  shared: {
+    react: {
+      singleton: true,
+    },
+    'react-dom': {
+      singleton: true,
+    },
+  },
+};
+
+module.exports = withStorybookModuleFederation(moduleFederationConfig)(
+  storybookConfig
+);
