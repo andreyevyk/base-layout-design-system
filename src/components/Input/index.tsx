0f85113820e8f00d5a4a6d5064df5aeabfb9fbe0
Andrey <andreyevyk13@gmail.com>
2022-02-03 16:55:33 -0300

    feat: change search color when has value

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,18 +1,43 @@
 import { Wrapper, WrapperProps } from "./styles";
 import { MdSearch } from 'react-icons/md';
 import useFocus from "hooks/useFocus";
+import { ChangeEvent, useMemo, useState } from "react";
+import { theme } from "config/stitches.config";
 
 export interface InputProps extends WrapperProps{
   inputClassName: string;
 }
 
-export const Input = ({className, inputClassName, css , ...rest}: InputProps) => {
+export const Input = ({
+  className, 
+  inputClassName, 
+  onChange, 
+  css , 
+  ...rest
+}: InputProps) => {
   const [inputRef, setInputFocus] = useFocus();
+  const [hasValue, setHasValue] = useState(false);
   
+  function onChangeInput(event: ChangeEvent<HTMLInputElement>) {
+    const { value } = event.currentTarget
+    setHasValue(!!value)
+    onChange && onChange(event);
+  }
+  const svgColor = useMemo(() => 
+    hasValue 
+      ? theme.colors.primary.toString() 
+      : theme.colors.complement.toString()
+  , [hasValue]);
+
   return (
     <Wrapper css={css} className={className}>
-        <MdSearch onClick={setInputFocus}/>
-        <input ref={inputRef} className={inputClassName}  {...rest}/>
+        <MdSearch color={svgColor} onClick={setInputFocus}/>
+        <input 
+          ref={inputRef} 
+          className={inputClassName} 
+          onChange={onChangeInput}
+          {...rest} 
+        />
     </Wrapper>
   );
 }
